9e55904e9ff91b7911ac494761002c20504653d9
luna-sakura <takmi.sakura@gmail.com>
2017-01-23 14:31:17 +0900

    Add files via upload

---
+++ Scripts/CharaManager.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharaManager : MonoBehaviour {
+
+	public GameObject Face1;
+	public GameObject Face2;
+	public GameObject Face3;
+	public GameObject Face4;
+	public GameObject Face5;
+
+	public int num;
+
+	// Use this for initialization
+	void Start () {
+		Face1.SetActive (false);
+		Face2.SetActive (false);
+		Face3.SetActive (false);
+		Face4.SetActive (false);
+		Face5.SetActive (false);
+
+		FaceChange (num);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void FaceChange(int x) {
+		switch (x) {
+		case 1:
+			Face1.SetActive (true);
+			break;
+
+		case 2:
+			Face2.SetActive (true);
+			break;
+
+		case 3:
+			Face3.SetActive (true);
+			break;
+
+		case 4:
+			Face4.SetActive (true);
+			break;
+
+		case 5:
+			Face5.SetActive (true);
+			break;
+		}
+	}
+}
---
+++ Scripts/TextController.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class TextController : MonoBehaviour 
+{
+	[Multiline] public string[] scenarios;
+	public string[] names;
+	public GameObject[] Character;
+
+	[SerializeField] Text uiText;
+	[SerializeField] Text nameText;
+
+	[SerializeField][Range(0.001f, 0.3f)]
+	float intervalForCharacterDisplay = 0.05f;
+
+	private string currentText = string.Empty;
+	private float timeUntilDisplay = 0;
+	private float timeElapsed = 1;
+	private int currentLine = 0;
+	private int lastUpdateCharacter = -1;
+
+	CharaManager charaManager;
+
+	// 文字の表示が完了しているかどうか
+	public bool IsCompleteDisplayText 
+	{
+		get { return  Time.time > timeElapsed + timeUntilDisplay; }
+	}
+
+	void Start()
+	{
+		SetNextLine();
+
+		charaManager = GetComponent<CharaManager>();
+	}
+
+	void Update () 
+	{
+		// 文字の表示が完了してるならクリック時に次の行を表示する
+		if( IsCompleteDisplayText ){
+			if(currentLine < scenarios.Length && Input.GetMouseButtonDown(0)){
+				SetNextLine();
+			}
+		}else{
+			// 完了してないなら文字をすべて表示する
+			if(Input.GetMouseButtonDown(0)){
+				timeUntilDisplay = 0;
+			}
+		}
+
+		int displayCharacterCount = (int)(Mathf.Clamp01((Time.time - timeElapsed) / timeUntilDisplay) * currentText.Length);
+		if( displayCharacterCount != lastUpdateCharacter ){
+			uiText.text = currentText.Substring(0, displayCharacterCount);
+			lastUpdateCharacter = displayCharacterCount;
+		}
+	}
+
+
+	void SetNextLine()
+	{
+		currentText = scenarios[currentLine];
+		nameText.text = names[currentLine];
+		timeUntilDisplay = currentText.Length * intervalForCharacterDisplay;
+		timeElapsed = Time.time;
+		currentLine ++;
+		lastUpdateCharacter = -1;
+	}
+}
